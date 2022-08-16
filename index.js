517b37e3675493ffa2225d8ec7febc7d09e27113
Bipro <biprobarai18@gmail.com>
2022-08-17 04:37:13 +0600

    enroll courses users

--- index.js
+++ index.js
@@ -25,7 +25,14 @@ async function run() {
     console.log("database connected");
 
     const database = client.db("ltechDB");
+
     const coursesCollection = database.collection("courses");
+
+    const enrollCollection = database.collection("enroll");
+
+    const usersCollection = database.collection("ussers");
+
+    const reviewCollection = database.collection("review");
 
     //GET fetch courses data
 
@@ -33,6 +40,119 @@ async function run() {
       const courses = coursesCollection.find({});
       const course = await courses.toArray();
       res.send(course);
+    });
+    // get single enroll
+    app.get("/enroll", async (req, res) => {
+      let query = {};
+      const email = req.query.email;
+      if (email) {
+        query = { email: email };
+      }
+      const cursor = enrollCollection.find(query);
+      const enroll = await cursor.toArray();
+      res.send(enroll);
+    });
+
+    // Get enroll
+    app.get("/enroll", async (req, res) => {
+      const enrolls = enrollCollection.find({});
+      const enroll = await enrolls.toArray();
+      res.send(enroll);
+    });
+
+    // Get Review
+    app.get("/review", async (req, res) => {
+      const reviews = reviewCollection.find({});
+      const review = await reviews.toArray();
+      res.send(review);
+    });
+    // Get users
+    app.get("/users", async (req, res) => {
+      const users = usersCollection.find({});
+      const user = await users.toArray();
+      res.send(user);
+    });
+    // get Admin
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role == "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+    // update
+
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+    // admin
+
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+
+    // POST courses
+
+    app.post("/courses", async (req, res) => {
+      const course = req.body;
+      const result = await coursesCollection.insertOne(course);
+      res.json(result);
+    });
+
+    // POST enrolling data
+
+    app.post("/enroll", async (req, res) => {
+      const enroll = req.body;
+      const result = await enrollCollection.insertOne(enroll);
+      res.json(result);
+    });
+
+    // Post users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+
+    // post review
+    app.post("/review", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.json(result);
+    });
+
+    // delete enrolment API
+    app.delete("/enroll/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await enrollCollection.deleteOne(query);
+      console.log("delete", result);
+      res.json(result);
+    });
+
+    // Delete course api
+    app.delete("/courses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await coursesCollection.deleteOne(query);
+      console.log("delete", result);
+      res.json(result);
     });
   } finally {
   }
